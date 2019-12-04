2c21647002bcb11e9da9b01ab755626aa3664f3a
Darren Ehlers <me@darrenehlers.com>
2019-12-04 09:48:43 -0600

    Updated DNSFramework package URLs

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/MarioIannotta/AtomicSwift.git", from: "1.2.1"),
-        .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.0.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.0.0"),
+        .package(url: "git@github.com:DoubleNode/DNSCoreThreading.git", from: "1.0.0"),
+        .package(url: "git@github.com:DoubleNode/DNSProtocols.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
