1af4e8eda1fba47e8a1408fe2441649996f1d381
Darren Ehlers <me@darrenehlers.com>
2020-01-10 10:25:29 -0600

    Removed AppGlobals (moved to DNSBaseStage due to Protocols dependency)

--- Package.swift
+++ Package.swift
@@ -21,14 +21,13 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/MarioIannotta/AtomicSwift.git", from: "1.2.1"),
         .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.0.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DNSCore",
-            dependencies: ["AtomicSwift", "DNSCoreThreading", "DNSProtocols"]),
+            dependencies: ["AtomicSwift", "DNSCoreThreading"]),
         .testTarget(
             name: "DNSCoreTests",
             dependencies: ["DNSCore"]),
