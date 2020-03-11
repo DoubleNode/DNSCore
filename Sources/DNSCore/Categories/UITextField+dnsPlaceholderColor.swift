a8c22b0cdf3404543b841b1a73d8f2ec69695400
Darren Ehlers <me@darrenehlers.com>
2020-03-11 18:04:13 -0500

    Added UITextField+dnsPlaceholderColor

---
+++ Sources/DNSCore/Categories/UITextField+dnsPlaceholderColor.swift
@@ -0,0 +1,23 @@
+//
+//  UITextField+dnsPlaceholderColor.swift
+//  DNSCore
+//
+//  Created by Darren Ehlers on 8/22/19.
+//  Copyright © 2019 DoubleNode.com. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    @IBInspectable var placeholderColor: UIColor {
+        get {
+            return attributedPlaceholder?.attribute(.foregroundColor, at: 0, effectiveRange: nil) as? UIColor ?? .clear
+        }
+        set {
+            guard let attributedPlaceholder = attributedPlaceholder else { return }
+            
+            let attributes: [NSAttributedString.Key: UIColor] = [.foregroundColor: newValue]
+            self.attributedPlaceholder = NSAttributedString(string: attributedPlaceholder.string, attributes: attributes)
+        }
+    }
+}
