26c3cd2f8aca14fad9d83ebf486017257a9310be
Darren Ehlers <me@darrenehlers.com>
2020-06-24 13:53:38 -0500

    Added TestViewController files

---
+++ Tests/DNSCoreTests/TestViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TestViewController.swift
+//  DNSCoreTests
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2020 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+}
