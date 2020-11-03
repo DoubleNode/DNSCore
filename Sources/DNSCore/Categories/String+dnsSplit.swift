a4084d131cacc8386bd517f567b9e4ed8766ee9f
Darren Ehlers <me@darrenehlers.com>
2020-11-02 22:19:08 -0600

    Added dnsSplit String extension

---
+++ Sources/DNSCore/Categories/String+dnsSplit.swift
@@ -0,0 +1,29 @@
+//
+//  String+dnsSplit.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2020 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    func dnsSplit(every length:Int) -> [Substring] {
+        guard length > 0 && length < count else {
+            return [suffix(from:startIndex)]
+        }
+        
+        return (0 ... (count - 1) / length)
+            .map { dropFirst($0 * length).prefix(length) }
+    }
+    func dnsSplit(backwardsEvery length:Int) -> [Substring] {
+        guard length > 0 && length < count else {
+            return [suffix(from:startIndex)]
+        }
+        
+        return (0 ... (count - 1) / length)
+            .map { dropLast($0 * length).suffix(length) }
+            .reversed()
+    }
+}
