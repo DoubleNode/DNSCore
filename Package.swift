893f4554d07898cb50228b12892515e92990e5ac
Darren Ehlers <me@darrenehlers.com>
2021-02-02 15:40:35 -0600

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/DoubleNodeOpen/AtomicSwift.git", from: "1.2.2"),
         .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.3.0"),
-        .package(url: "https://github.com/Nirma/SFSymbol", from: "0.5.1"),
+        .package(url: "https://github.com/Nirma/SFSymbol", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
