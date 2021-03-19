229d54aba4d2dbc4ac81d9a6a611ed3391a555a6
Darren Ehlers <me@darrenehlers.com>
2021-03-19 11:17:11 -0500

    Added string RegEx check

---
+++ Sources/DNSCore/Categories/String+dnsRegEx.swift
@@ -0,0 +1,17 @@
+//
+//  String+dnsRegEx.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2020 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import CommonCrypto
+import Foundation
+
+public extension String {
+    func dnsCheck(regEx: String) -> Bool {
+        let pred = NSPredicate(format:"SELF MATCHES %@", regEx)
+        return pred.evaluate(with: self)
+    }
+}
