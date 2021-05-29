c0a3c1158258bc145a8a0bf72f6392d0b7c4419c
Darren Ehlers <me@darrenehlers.com>
2021-05-29 09:22:29 -0500

    Readme update

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/DoubleNodeOpen/AtomicSwift.git", from: "1.2.2"),
-        .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.4.3"),
+        .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.5.0"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.5.0"),
         .package(url: "https://github.com/Nirma/SFSymbol", from: "1.0.0"),
     ],
