bcf40e49a742679da54afcbd9f0dbd0bc74a2eb8
Darren Ehlers <me@darrenehlers.com>
2021-06-02 16:26:55 -0500

    Updated DNSString

--- Sources/DNSCore/DNSString.swift
+++ Sources/DNSCore/DNSString.swift
@@ -11,16 +11,26 @@ import LocalAuthentication
 import UIKit
 
 public class DNSString: Hashable, Codable {
+    public enum Language: String, CaseIterable {
+        case en
+        case es419 = "es-419"
+    }
+
     private var _strings: [String: String] = [:]
 
     public var asString: String {
         _strings[DNSCore.languageCode] ?? ""
     }
-    public func asString(for languageCode: String) -> String {
-        _strings[DNSCore.languageCode] ?? ""
+    public func asString(for language: DNSString.Language) -> String {
+        _strings[language.rawValue] ?? ""
     }
-    public init(with strings: [String: String]) {
-        _strings = strings
+    public func asString(for languageStr: String) -> String {
+        _strings[languageStr] ?? ""
+    }
+    public init(with strings: [DNSString.Language: String] = [:]) {
+        var newStrings: [String: String] = [:]
+        strings.keys.forEach { newStrings[$0.rawValue] = strings[$0] }
+        _strings = newStrings
     }
     static public func == (lhs: DNSString, rhs: DNSString) -> Bool {
         return lhs.asString == rhs.asString
@@ -34,12 +44,12 @@ public class DNSString: Hashable, Codable {
     }
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        _strings["en"] = try container.decode(String.self, forKey: .en)
-        _strings["es-419"] = try container.decode(String.self, forKey: .es419)
+        _strings[Language.en.rawValue] = try container.decode(String.self, forKey: .en)
+        _strings[Language.es419.rawValue] = try container.decode(String.self, forKey: .es419)
     }
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(_strings["en"], forKey: .en)
-        try container.encode(_strings["es-419"], forKey: .es419)
+        try container.encode(_strings[Language.en.rawValue], forKey: .en)
+        try container.encode(_strings[Language.es419.rawValue], forKey: .es419)
     }
 }
