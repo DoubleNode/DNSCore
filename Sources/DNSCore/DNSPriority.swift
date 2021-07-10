517acd4d18e0828eea6ceb5765ed1053ef9bf6bd
Darren Ehlers <me@darrenehlers.com>
2021-07-10 16:42:56 -0500

    Added DNSPriority

---
+++ Sources/DNSCore/DNSPriority.swift
@@ -0,0 +1,17 @@
+//
+//  DNSPriority.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2021 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Foundation
+
+public enum DNSPriority {
+    static let none = 0
+    static let low = 250
+    static let normal = 500
+    static let high = 750
+    static let highest = 1000
+}
