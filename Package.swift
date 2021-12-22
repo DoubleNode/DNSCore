f7abce59931600721453a0649d7bdcc32b0fd624
Darren Ehlers <me@darrenehlers.com>
2021-12-22 15:22:23 -0600

    Updated platforms

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [
         .iOS(.v13),
-//        .tvOS(.v13),
-//        .macOS(.v10_15),
-//        .watchOS(.v6),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
