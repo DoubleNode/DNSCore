da7ac85d4abd72419b89dfbacd6a487a1e41f063
Darren Ehlers <me@darrenehlers.com>
2021-12-23 14:04:11 -0600

    Added URL extension

---
+++ Sources/DNSCore/Categories/URL+dnsBasedOn.swift
@@ -0,0 +1,22 @@
+//
+//  URL+dnsBasedOn.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2021 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Foundation
+
+public extension URL {
+    func basedOn(_ baseUrl: URL) -> URL? {
+        let renamingPath = self.absoluteString.replacingOccurrences(of: baseUrl.absoluteString,
+                                                                    with: "")
+        if isFileURL {
+            return URL(fileURLWithPath: renamingPath,
+                       relativeTo: baseUrl)
+        }
+        return URL(string: renamingPath,
+                   relativeTo: baseUrl)
+    }
+}
