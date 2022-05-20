5c0eca80e935df7e89315b1e0a3d8ceae060b644
Darren Ehlers <me@darrenehlers.com>
2022-05-20 12:35:18 -0500

    Added FloatingPoint extension to help with converting Float to Decimal in JSON output

---
+++ Sources/DNSCore/Categories/FloattingPoint+dnsLosslessStringConvertible.swift
@@ -0,0 +1,16 @@
+//
+//  FloattingPoint+dnsLosslessStringConvertible.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2021 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Foundation
+
+public extension LosslessStringConvertible {
+    var string: String { .init(self) }
+}
+public extension FloatingPoint where Self: LosslessStringConvertible {
+    var decimal: Decimal? { Decimal(string: string) }
+}
