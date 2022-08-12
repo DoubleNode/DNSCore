0d02fcbb68622cdc090aba90c6de5b965a8aa182
Darren Ehlers <me@darrenehlers.com>
2022-08-12 18:33:03 -0500

    Fix bugs with UIColor from dictionary

--- Sources/DNSCore/Categories/UIColor+dnsDictionary.swift
+++ Sources/DNSCore/Categories/UIColor+dnsDictionary.swift
@@ -22,7 +22,9 @@ public extension UIColor {
             // public init(white: CGFloat, alpha: CGFloat)
             let white = Self.xlt.double(from: data["white"] as Any?)
             let alpha = Self.xlt.double(from: data["alpha"] as Any?)
-            guard let white, let alpha else { return nil }
+            guard var white, var alpha else { return nil }
+            if white > 1 { white /= 256 }
+            if alpha > 1 { alpha /= 256 }
             self.init(white: white, alpha: alpha)
         } else if keys.contains("hue") {
             // public init(hue: CGFloat, saturation: CGFloat, brightness: CGFloat, alpha: CGFloat)
@@ -30,7 +32,11 @@ public extension UIColor {
             let saturation = Self.xlt.double(from: data["saturation"] as Any?)
             let brightness = Self.xlt.double(from: data["brightness"] as Any?)
             let alpha = Self.xlt.double(from: data["alpha"] as Any?)
-            guard let hue, let saturation, let brightness, let alpha else { return nil }
+            guard var hue, var saturation, var brightness, var alpha else { return nil }
+            if hue > 1 { hue /= 256 }
+            if saturation > 1 { saturation /= 256 }
+            if brightness > 1 { brightness /= 256 }
+            if alpha > 1 { alpha /= 256 }
             self.init(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
         } else if keys.contains("red") {
             // public init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
@@ -38,7 +44,11 @@ public extension UIColor {
             let green = Self.xlt.double(from: data["green"] as Any?)
             let blue = Self.xlt.double(from: data["blue"] as Any?)
             let alpha = Self.xlt.double(from: data["alpha"] as Any?)
-            guard let red, let green, let blue, let alpha else { return nil }
+            guard var red, var green, var blue, var alpha else { return nil }
+            if red > 1 { red /= 256 }
+            if green > 1 { green /= 256 }
+            if blue > 1 { blue /= 256 }
+            if alpha > 1 { alpha /= 256 }
             self.init(red: red, green: green, blue: blue, alpha: alpha)
         } else {
             return nil
