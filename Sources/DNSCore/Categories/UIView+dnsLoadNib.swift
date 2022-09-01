1b1d1c4e9090dabe9704a490e3d880ae9346081f
Darren Ehlers <me@darrenehlers.com>
2022-09-01 12:00:53 -0500

    Added dnsLoadNib

---
+++ Sources/DNSCore/Categories/UIView+dnsLoadNib.swift
@@ -0,0 +1,41 @@
+//
+//  UIView+dnsNib.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2022 - 2016 DoubleNode.com. All rights reserved.
+//
+
+#if !os(macOS)
+import UIKit
+
+public extension UIView {
+    @discardableResult
+    func dnsLoadNib<T: UIView>(from bundle: Bundle?) -> T? {
+        var bundle = bundle ?? Bundle.module
+        guard let contentView = bundle.loadNibNamed(String(describing: type(of: self)),
+                                                    owner: self, options: nil)?.first as? T else {
+            // xib not loaded, or its top view is of the wrong type
+            return nil
+        }
+        self.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.fixConstraintsInView(self)
+        return contentView
+    }
+    func fixConstraintsInView(_ container: UIView!) -> Void {
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal,
+                           toItem: container, attribute: .leading, multiplier: 1.0,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal,
+                           toItem: container, attribute: .trailing, multiplier: 1.0,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal,
+                           toItem: container, attribute: .top, multiplier: 1.0,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal,
+                           toItem: container, attribute: .bottom, multiplier: 1.0,
+                           constant: 0).isActive = true
+    }
+}
+#endif
