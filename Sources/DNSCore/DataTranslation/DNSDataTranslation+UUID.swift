b61f190df2542cffeef6bec71202794d35273ab4
Darren Ehlers <me@darrenehlers.com>
2022-10-08 20:06:14 -0500

    Another dataTranslator uuid tweak

--- Sources/DNSCore/DataTranslation/DNSDataTranslation+UUID.swift
+++ Sources/DNSCore/DataTranslation/DNSDataTranslation+UUID.swift
@@ -11,6 +11,12 @@ import Foundation
 
 public extension DNSDataTranslation {
     // MARK: - uuid...
+    func uuid<K>(from container: KeyedDecodingContainer<K>,
+                 forKey key: KeyedDecodingContainer<K>.Key) -> UUID? where K: CodingKey {
+        do { return uuid(from: try container.decodeIfPresent(UUID.self, forKey: key)) } catch { }
+        do { return uuid(from: try container.decodeIfPresent(String.self, forKey: key)) } catch { }
+        return nil
+    }
     // swiftlint:disable:next cyclomatic_complexity
     func uuid(from any: Any?) -> UUID? {
         guard let any else { return nil }
