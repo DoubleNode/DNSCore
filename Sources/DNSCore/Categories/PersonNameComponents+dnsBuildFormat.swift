3bea0959934451cb073e893ea199ccd23e820d39
Darren Ehlers <me@darrenehlers.com>
2022-11-13 19:04:04 -0600

    Tweaked PersonNameComponents category

--- Sources/DNSCore/Categories/PersonNameComponents+dnsBuildFormat.swift
+++ Sources/DNSCore/Categories/PersonNameComponents+dnsBuildFormat.swift
@@ -10,19 +10,65 @@ import Foundation
 
 public extension PersonNameComponents {
     static let xlt = DNSDataTranslation()
+    static func field(_ from: CodingKeys) -> String { return from.rawValue }
+    enum CodingKeys: String, CodingKey {
+        case familyName, givenName, middleName, namePrefix, nameSuffix, nickname
+    }
+
     init?(from data: DNSDataDictionary) {
         guard !data.isEmpty else { return nil }
-        let name = Self.xlt.string(from: data["long"] as Any?) ?? ""
-        let nickname =  Self.xlt.string(from: data["nickname"] as Any?) ?? ""
-        guard let retval = PersonNameComponents.dnsBuildName(with: name, and: nickname) else { return nil }
-        self = retval
+        self = PersonNameComponents()
+        self.familyName =  Self.xlt.string(from: data[Self.field(.familyName)] as Any?) ?? ""
+        self.givenName =  Self.xlt.string(from: data[Self.field(.givenName)] as Any?) ?? ""
+        self.middleName =  Self.xlt.string(from: data[Self.field(.middleName)] as Any?) ?? ""
+        self.namePrefix =  Self.xlt.string(from: data[Self.field(.namePrefix)] as Any?) ?? ""
+        self.nameSuffix =  Self.xlt.string(from: data[Self.field(.nameSuffix)] as Any?) ?? ""
+        self.nickname =  Self.xlt.string(from: data[Self.field(.nickname)] as Any?) ?? ""
     }
     var asDictionary: DNSDataDictionary {
         var retval = DNSDataDictionary()
-        retval.merge([
-            "long": self.dnsFormatName(style: .long),
-            "nickname": self.nickname ?? "",
-        ]) { (current, _) in current }
+        if let familyName = self.familyName {
+            if !familyName.isEmpty {
+                retval.merge([
+                    Self.field(.familyName): self.familyName ?? "",
+                ]) { (current, _) in current }
+            }
+        }
+        if let givenName = self.givenName {
+            if !givenName.isEmpty {
+                retval.merge([
+                    Self.field(.givenName): self.givenName ?? "",
+                ]) { (current, _) in current }
+            }
+        }
+        if let middleName = self.middleName {
+            if !middleName.isEmpty {
+                retval.merge([
+                    Self.field(.middleName): self.middleName ?? "",
+                ]) { (current, _) in current }
+            }
+        }
+        if let namePrefix = self.namePrefix {
+            if !namePrefix.isEmpty {
+                retval.merge([
+                    Self.field(.namePrefix): self.namePrefix ?? "",
+                ]) { (current, _) in current }
+            }
+        }
+        if let nameSuffix = self.nameSuffix {
+            if !nameSuffix.isEmpty {
+                retval.merge([
+                    Self.field(.nameSuffix): self.nameSuffix ?? "",
+                ]) { (current, _) in current }
+            }
+        }
+        if let nickname = self.nickname {
+            if !nickname.isEmpty {
+                retval.merge([
+                    Self.field(.nickname): self.nickname ?? "",
+                ]) { (current, _) in current }
+            }
+        }
         return retval
     }
     
