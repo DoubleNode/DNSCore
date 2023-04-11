46b0e5637e2f1b415918b19763f2c738dc81dfd4
Darren Ehlers <me@darrenehlers.com>
2023-04-11 12:18:51 -0500

    Added String subscript extension

---
+++ Sources/DNSCore/Categories/String+subscript.swift
@@ -0,0 +1,28 @@
+//
+//  String+subscript.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSCore
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2022 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    subscript(i: Int) -> String {
+        return self[i ..< i + 1]
+    }
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, count) ..< count]
+    }
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+    subscript(r: Range<Int>) -> String {
+        let range = Range(uncheckedBounds: (lower: max(0, min(count, r.lowerBound)),
+                                            upper: min(count, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+}
